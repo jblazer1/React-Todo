c79cc24916ecb3d2bcd732021a3e734aaee27ed9
JamesB <jblazer1@yahoo.com>
2019-03-05 19:09:04 -0800

    Add state to the App component and create the TodoList component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
+import Todo from "./components/TodoComponents/Todo";
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    };
+  }
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <Todo />
       </div>
     );
   }
